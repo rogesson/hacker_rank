9375bfa721a96bc780b3249408f2397a76ec14c8
rogesson <rogesson.barboza@locaweb.com.br>
2020-07-01 22:21:53 -0300

    day 00

---
+++ 30_days_of_code/golang/00_hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+  reader := bufio.NewReader(os.Stdin)
+  text, _ := reader.ReadString('\n')
+
+  fmt.Println("Hello, roger.")
+  fmt.Println(text)
+}
