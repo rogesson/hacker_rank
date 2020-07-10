3f231a8341ec563e817203e32531e05deb37f1a9
rogesson <rogessonb@gmail.com>
2020-07-09 22:57:43 -0300

    day 05 loops

---
+++ 30_days_of_code/golang/05_loops.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    var index int32
+
+    for index = 1; index <= 10; index += 1 {
+      result := n * index
+      fmt.Printf("%d x %d = %d\n", n, index, result)
+    }
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
