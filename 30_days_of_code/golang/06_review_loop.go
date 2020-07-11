af6b33b9b83d0bbf1ca22ce3d8d4a121e45c08eb
rogesson <rogessonb@gmail.com>
2020-07-11 16:37:22 -0300

    day 06 revew_loop

---
+++ 30_days_of_code/golang/06_review_loop.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+    "strconv"
+)
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+    count, _ := strconv.Atoi(readLine(reader))
+
+    for i := 0; i < count; i++ {
+        word := readLine(reader)
+        fmt.Println(parseWord(word))
+    }
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+func parseWord(word string) string {
+    var evenWords []string
+    var oddWords []string
+
+    for i, w := range strings.Split(word, "") {
+        if i % 2 == 0 {
+            evenWords = append(evenWords, w)
+        } else {
+            oddWords = append(oddWords, w)
+        }
+    }
+
+    strEvenWords := strings.Join(evenWords, "")
+    strOddWords := strings.Join(oddWords, "")
+
+    return fmt.Sprintf("%s %s", strEvenWords, strOddWords)
+}
+
