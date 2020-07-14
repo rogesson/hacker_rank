f361dcb18c994944925218108f2d7c19962caa81
rogesson <rogessonb@gmail.com>
2020-07-13 23:22:06 -0300

    day 08 maps from hell

---
+++ 30_days_of_code/golang/08_dictionaries_and_maps.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int(nTemp)
+
+    agenda := make(map[string]int)
+
+    for i := 0; i < n; i++ {
+        contact := readLine(reader)
+
+        saveContact(agenda, contact)
+    }
+
+    for i := n;; i++ {
+        name := readLine(reader)
+
+        if name == "" {
+            break
+        }
+
+        contact := findContact(agenda, name)
+        fmt.Println(contact)
+    }
+}
+
+func saveContact(agenda map[string]int, contact string) {
+    c := strings.Split(contact, " ")
+    name := c[0]
+    number, err := strconv.Atoi(c[1])
+    if err != nil {
+        fmt.Println("Number not valid %s\n", err)
+    }
+
+    agenda[name] = number
+}
+
+func findContact(agenda map[string]int, name string) string {
+    number := agenda[name]
+
+    if number == 0 {
+        return "Not found"
+    }
+
+    return fmt.Sprintf("%s=%d", name, number)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
